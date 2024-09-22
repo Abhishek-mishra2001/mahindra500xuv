6d35f45813ca5a94f8870d595ceadbc6e0659b22
Abhishek Mishra <abhishekmishra9865@gmail.com>
2024-09-22 15:34:04 +0530

    changes

--- javascript/mg-motor.js
+++ javascript/mg-motor.js
@@ -2,3 +2,7 @@ $(".listitmmain").click(function() {
     $(".listitmmain").removeClass("active-tab")
     $(this).addClass("active-tab")
 })
+$(".listitmmain").click(function() {
+    $(".listitms").removeClass("active-class")
+    $(this).find(".listitms").addClass("active-class");
+})
