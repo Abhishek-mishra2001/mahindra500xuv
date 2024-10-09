9207a2e3c0ea8bab6a257b7eb2f75d9ff923ca91
Abhishek Mishra <abhishekmishra9865@gmail.com>
2024-10-09 09:11:27 +0530

    changes

--- javascript/mg-motor.js
+++ javascript/mg-motor.js
@@ -1,4 +1,35 @@
-$('.listitmmain').click(function(event) {
-    $(".listitms").removeClass("active-class");
-    $(this).find($(".listitms")).addClass("active-class"); 
+// $('.listitmmain').click(function(event) {
+//     $(".listitms").removeClass("active-class");
+//     $(this).find($(".listitms")).addClass("active-class"); 
+// });
+
+$(document).ready(function(){
+    $(".accordion").click(function(){
+        // Toggle the active class on the clicked accordion
+        $(this).toggleClass("active");
+        
+        // Toggle between showing and hiding the panel
+        var panel = $(this).next(".panel");
+        panel.slideToggle();
+        
+        // Close other open panels
+        $(".panel").not(panel).slideUp();
+        $(".accordion").not(this).removeClass("active");
+    });
 });
+
+//     $('a[href^="#"]').off();
+//   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//       e.preventDefault();
+       
+//       const targetId = this.getAttribute('href');
+//       const targetElement = document.querySelector(targetId);
+//       const offset = 1000; // Adjust this to the height of your header
+//       window.scrollTo({
+//         top: targetElement.offsetTop - offset,
+//         behavior: 'smooth' // This enables smooth scrolling
+//       });
+//     });
+//   });
+
